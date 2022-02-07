d13ffc4b73a9682ab40701143979ee54ef467776
obakengshole <shole.obakeng@gmail.com>
2022-02-07 15:39:21 +0200

    Added react-bootstrap tabs

--- src/tech/pages/LaptopDetails.js
+++ src/tech/pages/LaptopDetails.js
@@ -10,8 +10,10 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+// import { Tab } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
+
+import { Tab, Tabs, TabContainer, TabContent, TabPane, Row, Col, Nav } from 'react-bootstrap'
 
 import { getLaptopDetailsById } from "../../services/api";
 import ProductDescription from "./ProductDescription";
@@ -19,37 +21,6 @@ import ProductDescription from "./ProductDescription";
 const LaptopDetails = () => {
   const { laptopId } = useParams()
   const laptopDetails = getLaptopDetailsById(laptopId);
-  
-  const [showProductDescription, setProductDescription] = useState(false)
-  const [showSpecs, setShowSpecs] = useState(false)
-  const [showSpecs2, setShowSpecs2] = useState(false)
-  const [showGamePerformanceStats, setShowGamePerformanceStats] = useState(false)
-
-  const [buttonActive, setButtonActive] = useState(false)
-
-  const handleShowSpecs = () => {
-    setShowSpecs(!showSpecs)
-    // setVariant(!variant)
-    setButtonActive(!buttonActive)
-    setProductDescription(false)
-    setShowGamePerformanceStats(false)
-  }
-  
-  const handleGamePerformance = () => {
-    setShowGamePerformanceStats(!showGamePerformanceStats)
-    // setVariant(!variant)
-    setButtonActive(!buttonActive)
-    setProductDescription(false)
-    setShowSpecs(false)
-  }
-  
-  const handleProductDescription = () => {
-    setProductDescription(!showProductDescription)
-    setButtonActive(!buttonActive)
-    // setVariant(!variant)
-    setShowSpecs(false)
-    setShowGamePerformanceStats(false)
-  }
   
   const navigate = useNavigate();
   const back = () => {
@@ -62,29 +33,17 @@ const LaptopDetails = () => {
         {" "}
         Back{" "}
       </Button>
-      <p />
-      <Stack spacing={5} direction="row">
-        <MyButton variant={buttonActive} action={handleProductDescription} text="Description"></MyButton>
-        <MyButton variant={buttonActive} action={handleShowSpecs} text="Specs">Specs</MyButton>
-        <MyButton variant={buttonActive} action={handleGamePerformance} text="Game Performance Stats"></MyButton>
-      </Stack>
 
       <p />
-      { showProductDescription && <ProductDescription /> }
-      { showSpecs && <Specs2 laptopDetails={laptopDetails} /> }
-      { showGamePerformanceStats && <GamePerformance /> }
+      <MyTabs
+        productDescription={<ProductDescription />}
+        techDetails={<Specs2 laptopDetails={laptopDetails} />}
+        gamePerformanceStats={<GamePerformance />}
+      />
 
-      {/* <Specs2 laptopDetails={laptopDetails} /> */}
     </div>
   );
 };
-
-const MyButton = (props) => {
-  const { active, action, text } = props
-  return (
-    <Button variant={ active ? "contained" : "outlined"} onClick={action}> {text} </Button>
-  )
-}
 
 const styleLabel = {
   width: "100%",
@@ -110,12 +69,12 @@ const Specs2 = (laptopDetails) => {
               return (
                 <>
                 <Grid item xs={1} sm={1} md={1}>
-                  <ListItem sx={{ bgcolor:"#9fd3c7", height: '100%' }}>
+                  <ListItem sx={{ bgcolor:"#ececec", height: '100%' }}>
                     <ListItemText primary={key} />
                   </ListItem>
                 </Grid>
                 <Grid item xs={1} sm={1} md={1}>
-                  <ListItem sx={{ bgcolor: "#ececec", height: '100%' }}>
+                  <ListItem sx={{ bgcolor: "#fafafa", height: '100%' }}>
                     <ListItemText primary={values[i]} />
                   </ListItem>
                 </Grid>
@@ -149,66 +108,28 @@ const GamePerformance = () => {
   )
 };
 
-const Specs = (laptopDetails) => {
-  console.log('laptopDetails', laptopDetails);
-  const laptopDetailsObj = laptopDetails.laptopDetails
-  const keys = Object.keys(laptopDetailsObj)
-  const values = Object.values(laptopDetailsObj)
-  console.log('keys', keys);
-  console.log('laptopDetailsObj', laptopDetailsObj);
-
+const MyTabs = (props) => {
   return (
-    <Grid container columns={{ xs: 2, sm: 8, md: 12 }}>
-      <Grid item xs={1} sm={4} md={4}>
-        <List sx={style} component="nav" aria-label="mailbox folders">
-
-        {
-          keys.map((key) => {
-            return (
-
-              <>
-            <ListItem button>
-            <ListItemText primary={key} />
-          </ListItem>
-            <Divider />
-            <p/>
-            </>
-              )
-          })
-        }
-
-        </List>
-      </Grid>
-
-      <Grid item xs={1} sm={4} md={4}>
-        <List sx={styleLabel} component="nav" aria-label="mailbox folders">
-
-          {
-            values.map((value) => {
-              return (
-                <>
-                  <ListItem>
-                    <ListItemText primary={value} />
-                    <ListItemText sx={{bgcolor:"#9fd3c7"}} primary={value} />
-                  </ListItem>
-                  <Divider />
-                  <p/>
-                </>
-              )
-            })
-          }
-
-        </List>
-      </Grid>
-    </Grid>
-  );
-};
-
-const style = {
-  width: "100%",
-  maxWidth: 360,
-  bgcolor: "#9fd3c7",
-  size: 10,
-};
+    <>
+    <Tabs
+  defaultActiveKey="prodDescription"
+  transition={false}
+  id="noanim-tab-example"
+  className="mb-3"
+  variant="pills"
+>
+  <Tab eventKey="prodDescription" title="Product Description">
+    { props.productDescription }
+  </Tab>
+  <Tab eventKey="techDetails" title="Technical Details">
+    { props.techDetails }
+  </Tab>
+  <Tab eventKey="gameStats" title="Game Performance Stats">
+    { props.gamePerformanceStats }
+  </Tab>
+</Tabs>
+</>
+  )
+}
 
 export default LaptopDetails;
